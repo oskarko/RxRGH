4c040d5930cc60411cd9448e8ac697a32f5cd450
Óscar Rodríguez <oscar.garrucho@gmail.com>
2020-09-26 09:47:30 +0200

    Initial Commit

---
+++ RGH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGH
+//
+//  Created by Oscar Rodriguez Garrucho on 26/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
